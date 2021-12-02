afc261ae5d32b21cef69a29396c1a1c3cc3f4e14
Arianna Carratù <>
2021-12-02 22:43:12 +0100

    Added the SumStackVariableOperation class.

---
+++ ProgrammableCalculator/src/stackvariableoperation/SumStackVariableOperation.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stackvariableoperation;
+
+import calculatorstack.CalculatorStack;
+import java.util.NoSuchElementException;
+import org.apache.commons.math3.complex.Complex;
+import variablesvector.VariablesVector;
+
+/**
+ *
+ * @author Arianna Carratù
+ */
+public class SumStackVariableOperation extends StackVariableOperation {
+    
+    /**
+     * Class Constructor
+     * @param calculatorStack instance of calculator's stack
+     * @param variablesVector instance of variables' vector
+     * @param variable the variable used to execute the operation
+     */
+    public SumStackVariableOperation(CalculatorStack calculatorStack, 
+            VariablesVector variablesVector, char variable){
+        super(calculatorStack, variablesVector, variable);
+    }
+
+    /**
+     * Sums the value of the given variable with the top element of the stack and pushes
+     * the result onto the stack. <br>
+     * If the stack is empty, it just pushes the variable onto the stack.
+     */
+    @Override
+    public void execute() {
+        int size = calculatorStack.size();
+        Complex variableNumber = variablesVector.getVarValue(variable);
+        Complex stackNumber = size == 0 ? Complex.ZERO : calculatorStack.pop();
+        Complex resultNumber = variableNumber.add(stackNumber);
+        calculatorStack.push(resultNumber);
+    }
+}
---
+++ ProgrammableCalculator/test/stackvariableoperation/SumStackVariableOperationTest.java
@@ -0,0 +1,218 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stackvariableoperation;
+
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import calculatorstack.CalculatorStack;
+import org.apache.commons.math3.complex.Complex;
+import variablesvector.VariablesVector;
+
+/**
+ *
+ * @author Arianna Carratù
+ */
+public class SumStackVariableOperationTest {
+    
+    private CalculatorStack calculatorStack;
+    private VariablesVector variablesVector;
+    private char variable;
+    private SumStackVariableOperation sumSVO;
+    
+    @Before
+    public void init(){
+        calculatorStack = new CalculatorStack();
+        variablesVector = new VariablesVector();
+        variable = 'a';
+        sumSVO = new SumStackVariableOperation(calculatorStack, variablesVector, variable);
+    }
+    
+    /**
+     * Test of execute method on empty stack
+     */
+    @Test
+    public void testExecuteEmptyStack() { 
+        System.out.println("testing the execution on an empty stack");
+        Complex variableNumber = new Complex(89, 42);
+        variablesVector.setVarValue(variable, variableNumber);
+        sumSVO.execute();
+        
+        Complex topNumber = calculatorStack.top();
+        assertEquals(topNumber, variableNumber); 
+    }
+    
+    /**
+     * Test of execute method between elements of from the first quadrant
+     */
+    @Test  
+    public void testExecuteFirstQuadrant(){ 
+        System.out.println("testing the  execution beetween elements of from the first quadrant");
+        Complex variableNumber = new Complex(1.0);
+        Complex stackNumber = new Complex(2.0, 1.0);
+        variablesVector.setVarValue(variable, variableNumber);
+        calculatorStack.push(stackNumber);
+        sumSVO.execute();
+        
+        Complex expectedNumber = new Complex(3.0, 1.0);
+        Complex topNumber = calculatorStack.top();
+        assertEquals(expectedNumber, topNumber);
+    }
+    /**
+     * Test of execute method between elements of from the first and second quadrant
+     */
+    @Test
+    public void testExecuteFirstSecondQuadrant(){ 
+        System.out.println("testing the  execution beetween elements of from the first and second quadrants"); 
+        Complex variableNumber = new Complex(1.0);
+        Complex stackNumber = new Complex(-1.0, 1.0);
+        variablesVector.setVarValue(variable, variableNumber);
+        calculatorStack.push(stackNumber);
+        sumSVO.execute();
+        
+        Complex expectedNumber = new Complex(0.0, 1.0);
+        Complex topNumber = calculatorStack.top();
+        assertEquals(expectedNumber, topNumber);
+    }
+
+    /**
+    * Test of execute method between elements of from the first and third quadrants
+    */
+    @Test
+    public void testExecuteFirstThirdQuadrant(){
+        System.out.println("testing the  execution beetween elements of from the first and third quadrants");
+        Complex variableNumber = new Complex(1.0);
+        Complex stackNumber = new Complex(-1.0, -1.0);
+        variablesVector.setVarValue(variable, variableNumber);
+        calculatorStack.push(stackNumber);
+        sumSVO.execute();
+        
+        Complex expectedNumber = new Complex(0.0, -1.0);
+        Complex topNumber = calculatorStack.top();
+        assertEquals(expectedNumber, topNumber);
+    }
+
+    /**
+     * Test of execute method between elements of from the first and fourth quadrants
+     */
+    @Test
+    public void testExecuteFirstFourthQuadrant(){
+        System.out.println("testing the  execution beetween elements of from the first and fourth quadrants");
+        Complex variableNumber = new Complex(1.0);
+        Complex stackNumber = new Complex(1.0, -1.0);
+        variablesVector.setVarValue(variable, variableNumber);
+        calculatorStack.push(stackNumber);
+        sumSVO.execute();
+        
+        Complex expectedNumber = new Complex(2.0, -1.0);
+        Complex topNumber = calculatorStack.top();
+        assertEquals(expectedNumber, topNumber);    
+    }
+
+    /**
+     * Test of execute method between elements of from the second quadrant
+     */
+    @Test
+    public void testExecuteSecondQuadrant(){
+        System.out.println("testing the  execution beetween elements of from the second quadrant");
+        Complex variableNumber = new Complex(-1.0, 3.0);
+        Complex stackNumber = new Complex(-2.0, 1.0);
+        variablesVector.setVarValue(variable, variableNumber);
+        calculatorStack.push(stackNumber);
+        sumSVO.execute();
+        
+        Complex expectedNumber = new Complex(-3.0, 4.0);
+        Complex topNumber = calculatorStack.top();
+        assertEquals(expectedNumber, topNumber);
+    }
+    
+    /**
+     * Test of execute method between elements of from the second and third quadrants
+     */
+    @Test 
+    public void testExecuteSecondThirdQuadrant(){ 
+        System.out.println("testing the  execution beetween elements of from the second and third quadrants");
+        Complex variableNumber = new Complex(-1.0, 3.0);
+        Complex stackNumber = new Complex(-2.0, -1.0);
+        variablesVector.setVarValue(variable, variableNumber);
+        calculatorStack.push(stackNumber);
+        sumSVO.execute();
+        
+        Complex expectedNumber = new Complex(-3.0, 2.0);
+        Complex topNumber = calculatorStack.top();
+        assertEquals(expectedNumber, topNumber);
+    }
+    
+    /**
+     * Test of execute method between elements of from the second and fourth quadrants
+     */
+    @Test
+    public void testExecuteSecondFourthQuadrant(){
+        System.out.println("testing the  execution beetween elements of from the second and fourth quadrants");
+        Complex variableNumber = new Complex(-1.0, 3.0);
+        Complex stackNumber = new Complex(2.0, -1.0);
+        variablesVector.setVarValue(variable, variableNumber);
+        calculatorStack.push(stackNumber);
+        sumSVO.execute();
+        
+        Complex expectedNumber = new Complex(1.0, 2.0);
+        Complex topNumber = calculatorStack.top();
+        assertEquals(expectedNumber, topNumber);
+    }
+    
+    /**
+     * Test of execute method between elements of from the third quadrant
+     */
+    @Test
+    public void testExecuteThirdQuadrant(){   
+        System.out.println("testing the  execution beetween elements of from the third quadrant");
+        Complex variableNumber = new Complex(-1.0, -2.0);
+        Complex stackNumber = new Complex(-2.0, -1.0);
+        variablesVector.setVarValue(variable, variableNumber);
+        calculatorStack.push(stackNumber);
+        sumSVO.execute();
+        
+        Complex expectedNumber = new Complex(-3.0, -3.0);
+        Complex topNumber = calculatorStack.top();
+        assertEquals(expectedNumber, topNumber);
+    }
+    
+    /**
+     * Test of execute method between elements of from the third and fourth quadrants
+     */
+    @Test
+    public void testExecuteThirdFourthQuadrant(){   
+        System.out.println("testing the  execution beetween elements of from the third and fourth quadrants");
+        Complex variableNumber = new Complex(-1.0, -2.0);
+        Complex stackNumber = new Complex(2.0, -1.0);
+        variablesVector.setVarValue(variable, variableNumber);
+        calculatorStack.push(stackNumber);
+        sumSVO.execute();
+        
+        Complex expectedNumber = new Complex(1.0, -3.0);
+        Complex topNumber = calculatorStack.top();
+        assertEquals(expectedNumber, topNumber);
+    }
+    
+    /**
+     * Test of execute method between elements of from the fourth quadrant
+     */
+    @Test
+    public void testExecuteFourthQuadrant(){
+        System.out.println("testing the  execution beetween elements of from the fourth quadrant");
+        Complex variableNumber = new Complex(3.0, -3.0);
+        Complex stackNumber = new Complex(2.0, -1.0);
+        variablesVector.setVarValue(variable, variableNumber);
+        calculatorStack.push(stackNumber);
+        sumSVO.execute();
+        
+        Complex expectedNumber = new Complex(5.0, -4.0);
+        Complex topNumber = calculatorStack.top();
+        assertEquals(expectedNumber, topNumber);
+    }
+    
+    
+}
