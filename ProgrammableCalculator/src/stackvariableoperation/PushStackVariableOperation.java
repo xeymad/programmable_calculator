9180c925ea08eb6971a95cdb73df223c72bdebdb
Matteo Di Rienzo <Utente@PC-Matteo>
2021-12-03 10:23:45 +0100

    Added PushStackVariableOperation class with relative test cases

---
+++ ProgrammableCalculator/src/stackvariableoperation/PushStackVariableOperation.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stackvariableoperation;
+
+import calculatorstack.CalculatorStack;
+import java.util.NoSuchElementException;
+import org.apache.commons.math3.complex.Complex;
+import variablesvector.VariablesVector;
+
+/**
+ *
+ * @author aferr
+ */
+public class PushStackVariableOperation extends StackVariableOperation{
+    
+    /**
+     * Class Constructor
+     * @param calculatorStack instance of calculator's stack
+     * @param variablesVector instance of variables' vector
+     * @param variable the variable used to execute the operation
+     */
+    public PushStackVariableOperation(CalculatorStack calculatorStack, 
+            VariablesVector variablesVector, char variable){
+        super(calculatorStack, variablesVector, variable);
+    }
+    
+    /**
+     * Set the previously given variable to stack's top element.
+     * If the stack is empty, the variable remains unchanged.
+     */
+    @Override
+    public void execute(){
+        calculatorStack.push(variablesVector.getVarValue(variable));    
+    }
+    
+}
---
+++ ProgrammableCalculator/test/stackvariableoperation/PushStackVariableOperationTest.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stackvariableoperation;
+
+import calculatorstack.CalculatorStack;
+import org.apache.commons.math3.complex.Complex;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import variablesvector.VariablesVector;
+
+/**
+ *
+ * @author Utente
+ */
+public class PushStackVariableOperationTest {
+    
+    private CalculatorStack calculatorStack;
+    private VariablesVector variablesVector;
+    private char variable;
+    private PushStackVariableOperation pushSVO;
+    
+    
+    @Before
+    public void init(){
+        calculatorStack = new CalculatorStack();
+        variablesVector = new VariablesVector();
+        variable = 'a';
+        pushSVO = new PushStackVariableOperation(calculatorStack, variablesVector, variable);
+    }
+    
+    /**
+     * Test if the variable's value is being pushed to stack's top.
+     */
+    @Test
+    public void testExecute(){
+        Complex z = new Complex(42, 0);
+        variablesVector.setVarValue(variable, z);
+        pushSVO.execute();
+        Complex insertedValue = calculatorStack.top();
+        assertEquals(insertedValue, z);
+    }
+    
+}
