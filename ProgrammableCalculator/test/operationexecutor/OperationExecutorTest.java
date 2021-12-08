79db95a71178d2e435237209bb1b5ee8ddcacc54
matteo <matteo@matteo-ThinkPad-T460s>
2021-12-08 19:36:21 +0100

    Added OperationExecutor test cases

---
+++ ProgrammableCalculator/test/operationexecutor/OperationExecutorTest.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operationexecutor;
+
+import calculatorstack.CalculatorStack;
+import org.apache.commons.math3.complex.Complex;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+import stackoperation.*;
+
+
+/**
+ *
+ * @author matteo
+ */
+public class OperationExecutorTest {
+    
+    private OperationExecutor opExecutor;
+    private CalculatorStack calculatorStack;
+    
+    @Before
+    public void init() {
+        calculatorStack = new CalculatorStack();
+    }
+    /**
+    * Tests the execution with the empty Operation
+    */
+    @Test(expected = UndefinedOperationException.class)
+    public void testOperationExecutorEmptyConstructor() {
+        opExecutor = new OperationExecutor();
+        opExecutor.execute();
+    }
+    
+    /**
+     * Test the non empty constructor of the OperationExecutor
+     */
+    @Test
+    public void testOperationExecutorNonEmptyConstructor() {
+        StackOperation op = new PushStackOperation(calculatorStack, new Complex(42,42)); 
+        opExecutor = new OperationExecutor(op);
+        opExecutor.execute();
+        assertEquals(calculatorStack.pop(), new Complex(42, 42));
+    }
+    
+    /**
+     * Tests the Setter of the OperatorExecutor
+     */
+    @Test
+    public void testOperationExecutorSet() {
+        StackOperation op = new PushStackOperation(calculatorStack, new Complex(42,42)); 
+        opExecutor = new OperationExecutor();
+        opExecutor.setCommand(op);
+        opExecutor.execute();
+        assertEquals(calculatorStack.pop(), new Complex(42, 42));
+    }
+    
+}
